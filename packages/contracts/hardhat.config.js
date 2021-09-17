938ddbd91700e1f31d50e743d500ee2069076fc9
Stefan <stadolf@gmail.com>
2021-09-17 20:46:17 +0200

    adds some default anchors to hh config

--- packages/contracts/hardhat.config.js
+++ packages/contracts/hardhat.config.js
@@ -17,5 +17,13 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    // rinkeby: {
+    //   url: "",
+    //   accounts: [privateKey1, privateKey2, ...]
+    // }
+  },
   solidity: "0.8.4",
 };
