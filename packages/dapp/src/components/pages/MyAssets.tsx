1732e43630a2a372b48250c203d9ece36c4bf80b
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2021-09-25 02:42:18 +0200

    color extraction state based NFT item / color display adds library typings

--- packages/dapp/src/components/pages/MyAssets.tsx
+++ packages/dapp/src/components/pages/MyAssets.tsx
@@ -16,10 +16,24 @@ import { getNFTs } from '../../modules/nftport';
 import { getNFTs as readNFTsFromChain } from '../../modules/chain';
 import { NFTItem } from '../../types/NFTPort';
 import { truncateAddress } from '../../modules/strings';
+import ImageToColors, { Color } from 'image-to-colors';
+import palette from 'get-rgba-palette';
+import rgbHex from 'rgb-hex';
+
+enum MintingState {
+  NOT_MINTED,
+  GETTING_COLORS,
+  GOT_COLORS,
+  MINTING,
+  MINTED
+}
 
 export const NFTCard = ({ nft }: { nft: NFTItem }) => {
   if (!nft.metadata) return <></>;
-
+  const [dominantColors, setDominantColors] = useState<Color[]>([]);
+  const [mintingState, setMintingState] = useState<MintingState>(
+    MintingState.NOT_MINTED
+  );
   const { image, image_url } = nft.metadata;
 
   let imgSrc = image ? image : image_url;
@@ -27,6 +41,27 @@ export const NFTCard = ({ nft }: { nft: NFTItem }) => {
   if (imgSrc?.startsWith('ipfs://')) {
     imgSrc = imgSrc.replace('ipfs://', 'http://ipfs.io/');
   }
+
+  const extractColors = async () => {
+    setMintingState(MintingState.GETTING_COLORS);
+    const _pixels = await ImageToColors.getFromExternalSource(imgSrc!, {
+      setImageCrossOriginToAnonymous: true
+    });
+    const flatPixels = _pixels.flatMap((p) => [...p, 255]);
+    const colors = palette(flatPixels, 10);
+    setDominantColors(colors);
+    setMintingState(MintingState.GOT_COLORS);
+  };
+
+  const doMint = async () => {
+    setMintingState(MintingState.MINTING);
+
+    setMintingState(MintingState.MINTED);
+  };
+
+  const buzy = [MintingState.GETTING_COLORS, MintingState.MINTING].includes(
+    mintingState
+  );
   return (
     <Flex
       rounded="2xl"
@@ -41,14 +76,31 @@ export const NFTCard = ({ nft }: { nft: NFTItem }) => {
       boxShadow="rgba(0, 0, 0, 0.05) 0px 10px 20px 0px"
       justify="space-between"
     >
-      <Image
-        src={imgSrc}
-        title={imgSrc}
-        boxSize="fit-content"
-        objectFit="cover"
-        alt={imgSrc}
-        fallbackSrc="https://via.placeholder.com/800"
-      />
+      <Flex direction="row">
+        {dominantColors.length > 0 ? (
+          <Flex direction="column" h="100%" justify="space-between" minW={15}>
+            {dominantColors.map((color) => (
+              <Flex
+                flex="1 1 0px"
+                background={`#${rgbHex(color[0], color[1], color[2])}`}
+              >
+                &nbsp;
+              </Flex>
+            ))}
+          </Flex>
+        ) : (
+          <></>
+        )}
+        <Image
+          src={imgSrc}
+          title={imgSrc}
+          boxSize="fit-content"
+          objectFit="cover"
+          alt={imgSrc}
+          fallbackSrc="https://via.placeholder.com/800"
+          opacity={buzy ? 0.2 : 1}
+        />
+      </Flex>
       <Box p={4}>
         <Heading size="lg">{nft.metadata?.name}</Heading>
       </Box>
@@ -60,7 +112,17 @@ export const NFTCard = ({ nft }: { nft: NFTItem }) => {
           <Text color="white">{truncateAddress(nft.contract_address)}</Text>
         </Flex>
         <Spacer />
-        <Button variant="solid">Mint</Button>
+        {mintingState != MintingState.MINTED && (
+          <Button
+            variant="solid"
+            onClick={
+              mintingState === MintingState.GOT_COLORS ? doMint : extractColors
+            }
+            disabled={buzy}
+          >
+            {mintingState === MintingState.NOT_MINTED ? 'Get Colors' : 'Mint'}
+          </Button>
+        )}
       </Flex>
     </Flex>
   );
