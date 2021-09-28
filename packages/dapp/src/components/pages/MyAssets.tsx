e11c04a986172818dd3e4b57a8c791d70b750560
Stefan <stadolf@gmail.com>
2021-09-29 01:17:13 +0200

    error handling

--- packages/dapp/src/components/pages/MyAssets.tsx
+++ packages/dapp/src/components/pages/MyAssets.tsx
@@ -1,10 +1,10 @@
-import { Container, SimpleGrid, Box } from '@chakra-ui/react';
+import { Container, SimpleGrid, Box, useToast, Alert } from '@chakra-ui/react';
 import { useWeb3React } from '@web3-react/core';
-import { ethers, providers } from 'ethers';
+import { providers } from 'ethers';
 import React, { useEffect, useState } from 'react';
 import { CHAINS, getAllAssetsOfOwner } from '../../modules/chain';
 import { getNFTs } from '../../modules/nftport';
-import * as Splice from '../../modules/splice';
+
 import { NFTItem } from '../../types/NFTPort';
 import { NFTCard } from '../molecules/NFTCard';
 
@@ -12,7 +12,7 @@ export const MyAssetsPage = () => {
   const { account, library, chainId } = useWeb3React<providers.Web3Provider>();
 
   const [nfts, setNFTs] = useState<NFTItem[]>();
-
+  const toast = useToast();
   const fetchAssets = async () => {
     //todo: either use global state or cache the assets somehow.
 
@@ -31,11 +31,29 @@ export const MyAssetsPage = () => {
   };
 
   useEffect(() => {
-    fetchAssets();
+    (async () => {
+      try {
+        await fetchAssets();
+        toast({
+          status: 'success',
+          title: 'fetched all assets'
+        });
+      } catch (e) {
+        toast({
+          status: 'error',
+          title: "couldn't fetch assets"
+        });
+      }
+    })();
   }, [account, chainId]);
 
   return nfts ? (
     <Container maxW="container.xl">
+      {nfts.length === 0 && (
+        <Alert status="info">
+          it seems you don't have any assets on chain {chainId}{' '}
+        </Alert>
+      )}
       <SimpleGrid columns={[2, null, 3]} spacingX="40px" spacingY="20px">
         {nfts.map((nft) => (
           <NFTCard key={`${nft.contract_address}/${nft.token_id}`} nft={nft} />
