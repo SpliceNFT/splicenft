f9cd20ce07b9a8964c1c4621b40874d55de8f868
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2021-10-06 22:26:27 +0200

    a micro compatible server side renderer

---
+++ packages/backend/api/render.js
@@ -0,0 +1,17 @@
+const p5 = require('p5js-node');
+
+module.exports = (req, res) => {
+  new p5((p) => {
+    p.setup = function () {
+      p.createCanvas(1920, 1080);
+    };
+
+    p.draw = function () {
+      p.background(0);
+      p.fill(80);
+      p.rect(10, 10, 50, 50);
+      res.writeHead(200, { 'Content-Type': 'image/png' });
+      res.end(p.canvas.toBuffer());
+    };
+  });
+};
