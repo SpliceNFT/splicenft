7d928c5fa7079cba0526f912db84c50e48a2559d
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2021-10-06 16:28:56 +0200

    deployment scripts / rinkeby deployment

--- packages/contracts/scripts/deploySplice.ts
+++ packages/contracts/scripts/deploySplice.ts
@@ -1,22 +1,15 @@
 import { ethers, upgrades } from 'hardhat';
 
 (async () => {
-  await (async () => {
-    const Splice = await ethers.getContractFactory('Splice', {
-      // we might need to deploy that separately because of size
-      // libraries: {
-      //   Base58: '0xA51c1fc2f0D1a1b8494Ed1FE312d7C3a78Ed91C0'
-      // }
-    });
-    const splice = await upgrades.deployProxy(Splice, [
-      'Splice',
-      'SPLICE',
-      'ipfs://',
-      10000
-    ]);
+  const Splice = await ethers.getContractFactory('Splice', {
+    // we might need to deploy that separately because of size
+    // libraries: {
+    //   Base58: '0xA51c1fc2f0D1a1b8494Ed1FE312d7C3a78Ed91C0'
+    // }
+  });
+  const splice = await upgrades.deployProxy(Splice, ['Splice', 'SPLICE']);
 
-    //await splice.deployed();
+  //await splice.deployed();
 
-    console.log('Deployed splice contracts:', splice.address);
-  })();
+  console.log('Deployed splice contracts:', splice.address);
 })();
