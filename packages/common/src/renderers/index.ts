a07b1c18c77bfca8442c34066e77c77f6334f0ca
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2021-10-07 22:50:05 +0200

    fixes p5 code for js. adds District1618 renderer

--- packages/common/src/renderers/index.ts
+++ packages/common/src/renderers/index.ts
@@ -1,12 +1,14 @@
 import { default as Confidence } from './ConfidenceInTheMission';
 import { default as Garden } from './TheGardenOfEarthlyDelights';
+import { default as District1618 } from './District1618';
 import { default as Flower } from './Flower';
 import { Renderer } from '../types/Renderers';
 
 export const Renderers: Record<string, Renderer> = {
   Flower: Flower,
   'Confidence In the Mission': Confidence,
-  'The Garden of Earthly Delights': Garden
+  'The Garden of Earthly Delights': Garden,
+  District1618: District1618
 };
 
 export const RendererNames = Object.keys(Renderers);
