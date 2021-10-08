9100233f813c9556e8465cc285209d3e3775c6aa
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2021-10-08 23:47:51 +0200

    adds BAYC on kovan as testnet nft

--- packages/dapp/src/modules/chain.ts
+++ packages/dapp/src/modules/chain.ts
@@ -18,7 +18,10 @@ const knownContracts: Record<ChainOpt, string[]> = {
     '0xF5aa8981E44a0F218B260C99F9C89Ff7C833D36e', //CC
     '0xe85C716577A58d637ddA647caf42Bc5a6cBA2e95' //SSS
   ],
-  kovan: ['0x6334d2cbC3294577BB9de58e8b1901d6e3b97681'],
+  kovan: [
+    '0x6334d2cbC3294577BB9de58e8b1901d6e3b97681', //CC
+    '0x6d96aAE79399C6f2630d585BBb0FCF31cCa88fa9' //BAYC
+  ],
   localhost: (
     process.env.REACT_APP_TESTNETNFT_CONTRACT_ADDRESS as string
   ).split(',')
