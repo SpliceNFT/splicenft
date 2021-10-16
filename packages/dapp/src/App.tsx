208bf84d16dd1abc755869bc3d72986eb8c2b69b
Stefan <stadolf@gmail.com>
2021-10-16 12:50:52 +0200

    React Helmet og information & artwork

--- packages/dapp/src/App.tsx
+++ packages/dapp/src/App.tsx
@@ -1,6 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { Web3ReactProvider } from '@web3-react/core';
 import { providers } from 'ethers';
+import { Helmet } from 'react-helmet';
 import React from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import { Footer } from './components/molecules/Footer';
@@ -17,11 +18,41 @@ function getLibrary(provider: any) {
   return new providers.Web3Provider(provider);
 }
 
+const SITE_DESCRIPTION =
+  'Splice is a protocol for generative art based on origin NFTs. Our first usecase is to mint header images for your PFP NFTs.';
+
+const SITE_TITLE = 'S P L I C E - NFT artwork minter';
+
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Web3ReactProvider getLibrary={getLibrary}>
         <SpliceProvider>
+          <Helmet>
+            <meta charSet="utf-8" />
+            <meta name="description" content={SITE_DESCRIPTION} />
+            <meta
+              name="keywords"
+              content="web3,dapp,nfts,generative art,ethereum"
+            />
+            <meta property="og:title" content={SITE_TITLE} />
+            <meta property="og:type" content="Website" />
+            <meta property="og:url" content="https://getsplice.io" />
+            <meta property="og:description" content={SITE_DESCRIPTION} />
+            <meta
+              property="og:image"
+              content="https://getsplice.io/splice_opengraph.jpg"
+            />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:site" content="@splicenft" />
+            <meta name="twitter:title" content={SITE_TITLE} />
+            <meta name="twitter:description" content={SITE_DESCRIPTION} />
+            <meta
+              name="twitter:image"
+              content="https://getsplice.io/splice_opengraph.jpg"
+            />
+            <link rel="canonical" href="https://getsplice.io" />
+          </Helmet>
           <Router>
             <Header />
 
