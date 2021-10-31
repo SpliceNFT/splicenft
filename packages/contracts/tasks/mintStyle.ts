b4d1c55b0dad952fceed2a30ce2457ec7401bb7e
Stefan <stadolf@gmail.com>
2021-10-31 23:07:02 +0100

    style minting relies on conventional directory layout

--- packages/contracts/tasks/mintStyle.ts
+++ packages/contracts/tasks/mintStyle.ts
@@ -3,14 +3,13 @@ import { HardhatUserConfig, task } from 'hardhat/config';
 import '@nomiclabs/hardhat-ethers';
 import fs from 'fs';
 
-//pnpx hardhat --network localhost splice:style --account-idx 18 --style-nft-address 0x9A9f2CCfdE556A7E9Ff0848998Aa4a0CFD8863AE --price-strategy-address 0x959922bE3CAee4b8Cd9a407cc3ac1C251C2007B1  ../../renderers/ConfidenceInTheMission.js ../../renderers/ConfidenceInTheMission.png 0.05 200
+//pnpx hardhat --network localhost splice:style --account-idx 18 --style-nft-address 0x9A9f2CCfdE556A7E9Ff0848998Aa4a0CFD8863AE --price-strategy-address 0x959922bE3CAee4b8Cd9a407cc3ac1C251C2007B1  ../../renderers/ConfidenceInTheMission 0.05 200
 
 task('splice:style', 'mints a style')
   .addParam('styleNftAddress')
   .addParam('priceStrategyAddress')
   .addOptionalParam('accountIdx', "the artist's account index", '0')
-  .addPositionalParam('codePath')
-  .addPositionalParam('stylePreviewImagePath')
+  .addPositionalParam('directory')
   .addPositionalParam('mintPriceEth')
   .addPositionalParam('cap')
 
@@ -19,8 +18,7 @@ task('splice:style', 'mints a style')
       styleNftAddress,
       priceStrategyAddress,
       accountIdx,
-      codePath,
-      stylePreviewImagePath,
+      directory,
       mintPriceEth,
       cap
     } = taskArgs;
@@ -32,32 +30,27 @@ task('splice:style', 'mints a style')
     const StyleNFT = await hre.ethers.getContractFactory('SpliceStyleNFTV1');
     const styleNFT = await StyleNFT.attach(styleNftAddress);
 
-    const code = await fs.promises.readFile(codePath);
-    const previewImg = await fs.promises.readFile(stylePreviewImagePath);
+    const artistMetadata = JSON.parse(
+      await fs.promises.readFile(`${directory}/metadata.json`, 'utf-8')
+    );
+    const code = await fs.promises.readFile(`${directory}/code.js`);
+    const previewImg = await fs.promises.readFile(`${directory}/preview.png`);
+
+    const _metadata = { ...artistMetadata };
+
+    _metadata.image = new File([previewImg], 'preview.png', {
+      type: 'image/png'
+    });
+
+    _metadata.properties.code = new File([code], 'code.js', {
+      type: 'application/javascript'
+    });
 
     const nftStorageClient = new NFTStorage({
       token: process.env.NFTSTORAGE_APIKEY as string
     });
 
-    const metadata = await nftStorageClient.store({
-      name: 'ConfidenceInTheMission',
-      description: `ConfidenceInTheMission generates an abstraction of the interior of HAL, from 2001 Space Odyssey.
-       It draws two grids of rectangles on top of one another:
-       1. a main grid that angles with one-point perspective, has a wide dark gap in the vertical middle,
-       and fades in towards the vertical middle of the canvas and
-       2. a grid that's flat, fully saturated, and displays blocks of rects of various colors.
-      `,
-      image: new File([previewImg], 'preview.png', { type: 'image/png' }),
-      properties: {
-        creator_name: 'Emily Weil',
-        creator_twitter: '@emilyaweil',
-        code_library: 'p5.js',
-        code_library_version: '1.4.0',
-        code: new File([code], 'code.js', {
-          type: 'application/javascript'
-        })
-      }
-    });
+    const metadata = await nftStorageClient.store(_metadata);
 
     const cid = metadata.ipnft;
     console.debug('uploaded metadata', metadata.embed());
