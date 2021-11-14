3798c3048f8a4a0842909057ac779efdd6a50b81
Stefan <stadolf@gmail.com>
2021-11-15 00:09:30 +0100

    add a reservation feature per style #104 removes splice style interface reservations are verified by merkle proofs

--- packages/contracts/contracts/StyleSettings.sol
+++ packages/contracts/contracts/StyleSettings.sol
@@ -3,10 +3,26 @@
 pragma solidity 0.8.10;
 import './ISplicePriceStrategy.sol';
 
+enum MintingPhase {
+  Closed,
+  Presale,
+  Public
+}
+
+struct Allowlist {
+  //counting down
+  uint32 numReserved;
+  bytes32 merkleRoot;
+  uint64 reservedUntil;
+  uint8 mintsPerAddress;
+}
+
 struct StyleSettings {
   uint32 cap;
   string styleCID;
   ISplicePriceStrategy priceStrategy;
   bytes32 priceParameters;
+  bool salesIsActive;
+  //counting up
   uint32 mintedOfStyle;
 }
