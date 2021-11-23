559a0a57ad2815c78585e6aa51220e3a24f2143c
Stefan <stadolf@gmail.com>
2021-11-23 23:44:00 +0100

    #120 adds styles to subgraph schema associates splices & styles adds origin metadata url to subgraph

--- packages/subgraph/src/mapping.ts
+++ packages/subgraph/src/mapping.ts
@@ -1,29 +1,41 @@
-import { ethereum, Bytes, log as glog } from '@graphprotocol/graph-ts';
-import { Splice } from '../generated/schema';
 import {
-  Approval,
-  ApprovalForAll,
+  BigInt,
+  Bytes,
+  ethereum,
+  Address,
+  log as glog
+} from '@graphprotocol/graph-ts';
+import { Splice, Style } from '../generated/schema';
+import { ERC721 as ERC721Contract } from '../generated/Splice/ERC721';
+import {
   Minted,
-  OwnershipTransferred,
-  Paused,
-  SharesChanged,
+  Splice as SpliceContract,
   Transfer,
-  Unpaused,
   Withdrawn
 } from '../generated/Splice/Splice';
+
+import {
+  Minted as StyleMinted,
+  Transfer as StyleTransferred,
+  SpliceStyleNFT as StyleNFTContract,
+  SpliceStyleNFT
+} from '../generated/SpliceStyleNFT/SpliceStyleNFT';
 
 export function handleMinted(event: Minted): void {
   const tokenId = event.params.token_id.toString();
   const splice = new Splice(tokenId);
 
+  const spliceContract = SpliceContract.bind(event.address);
+  splice.metadata_url = spliceContract.tokenURI(event.params.token_id);
+
   splice.owner = event.transaction.from;
-  splice.style_token_id = event.params.style_token_id.toU32();
   splice.origin_hash = event.params.origin_hash;
 
   //1st 4 byte = method keccak hash fragment
   //3*bytes32 (96 bytes) = values / function args
   const txInput = event.transaction.input.subarray(4, 100);
   const reducedInput = Bytes.fromUint8Array(txInput);
+  splice.minting_fee = event.transaction.value;
 
   glog.debug('tx input: {}', [reducedInput.toHexString()]);
 
@@ -34,31 +46,62 @@ export function handleMinted(event: Minted): void {
 
   if (decoded) {
     const t = decoded.toTuple();
-    splice.origin_collection = t[0].toAddress();
+    const originAddress = t[0].toAddress();
+    splice.origin_collection = originAddress;
     splice.origin_token_id = t[1].toBigInt();
+    if (originAddress && splice.origin_token_id) {
+      const originContract = ERC721Contract.bind(originAddress);
+      splice.origin_metadata_url = originContract.tokenURI(
+        splice.origin_token_id as BigInt
+      );
+    }
   }
+  const style_id = event.params.style_token_id.toString();
+  splice.style = style_id;
+  const style = Style.load(style_id);
+  if (style) {
+    style.minted = style.minted + 1;
+    style.save();
+  }
+
   splice.save();
 }
 
 export function handleTransfer(event: Transfer): void {
   const splice = Splice.load(event.params.tokenId.toString());
   if (!splice) return;
+
+  const spliceContract = SpliceContract.bind(event.address);
+  splice.metadata_url = spliceContract.tokenURI(BigInt.fromString(splice.id));
+
   splice.owner = event.params.to;
   splice.save();
 }
 
-export function handleApproval(event: Approval): void {}
-
 export function handleWithdrawn(event: Withdrawn): void {}
 
-// undhandled:
+export function handleStyleMinted(event: StyleMinted): void {
+  const style_id = event.params.style_token_id.toString();
+  let style = Style.load(style_id);
+  if (!style) {
+    style = new Style(style_id);
+  }
 
-export function handleApprovalForAll(event: ApprovalForAll): void {}
+  style.cap = event.params.cap.toU32();
+  style.minted = 0;
+  const styleContract = StyleNFTContract.bind(event.address);
+  style.metadata_url = styleContract.tokenURI(event.params.style_token_id);
+  //const settings = styleContract.getSettings(event.params.style_token_id);
+  style.save();
+}
 
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+export function handleStyleTransferred(event: StyleTransferred): void {
+  const style_id = event.params.tokenId.toString();
+  let style = Style.load(style_id);
+  if (!style) {
+    style = new Style(style_id);
+  }
 
-export function handlePaused(event: Paused): void {}
-
-export function handleSharesChanged(event: SharesChanged): void {}
-
-export function handleUnpaused(event: Unpaused): void {}
+  style.owner = event.params.to;
+  style.save();
+}
