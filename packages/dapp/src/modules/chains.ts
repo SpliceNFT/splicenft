48dd59a1208353995b2b74c3ddd1f69d376e6658
Stefan <stadolf@gmail.com>
2021-12-02 23:09:39 +0100

    removes SSS and BAYC from "known NFTs" on Rinkeby (metadata unresponsive)

--- packages/dapp/src/modules/chains.ts
+++ packages/dapp/src/modules/chains.ts
@@ -7,8 +7,6 @@ export const knownCollections: Record<ChainOpt, string[]> = {
   ethereum: [],
   rinkeby: [
     '0xF5aa8981E44a0F218B260C99F9C89Ff7C833D36e', //CC
-    '0xe85C716577A58d637ddA647caf42Bc5a6cBA2e95', //SSS
-    '0xf4E7Dc1f4409dEb6da70486A1677d936Db52483b', //BAYC
     '0xd56c266c640F406db3B02C7054d2848252beE664', //DOODLE
     '0x25a1e017Ead38e2267593cE116d1E272Ba4D12c0', //Robotos
     '0xAF7209354132a3eab58D5CDef6FA28D98A27222E', //Lazy Lions
