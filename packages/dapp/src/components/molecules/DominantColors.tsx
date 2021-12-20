e41abd60edbfa7510971a9e2bdb8bc6715b8f4bb
Stefan <stadolf@gmail.com>
2021-12-20 18:11:28 +0100

    add a clipboard copy btn to the extracted color palette for style devs #161

--- packages/dapp/src/components/molecules/DominantColors.tsx
+++ packages/dapp/src/components/molecules/DominantColors.tsx
@@ -1,23 +1,46 @@
-import { Flex, Skeleton, Text } from '@chakra-ui/react';
+import {
+  Flex,
+  IconButton,
+  Skeleton,
+  Text,
+  useClipboard
+} from '@chakra-ui/react';
 import { extractColors } from '@splicenft/common';
 import { RGB } from 'get-rgba-palette';
 import React, { useEffect } from 'react';
+import { FaClipboard, FaClipboardCheck } from 'react-icons/fa';
 import rgbHex from 'rgb-hex';
 
-export const DominantColorsDisplay = ({ colors }: { colors: RGB[] }) => (
-  <Skeleton isLoaded={colors.length > 0} w="70%" size="lg">
-    <Flex direction="row" align="center" height="1.5em" gridGap={0}>
-      {colors.map((color) => {
-        const colorHex = `#${rgbHex(color[0], color[1], color[2])}`;
-        return (
-          <Flex key={colorHex} flex="1" background={colorHex}>
-            &nbsp;
-          </Flex>
-        );
-      })}
-    </Flex>
-  </Skeleton>
-);
+export const DominantColorsDisplay = ({ colors }: { colors: RGB[] }) => {
+  const { hasCopied, onCopy } = useClipboard(JSON.stringify(colors));
+
+  return (
+    <Skeleton isLoaded={colors.length > 0} w="70%" size="lg">
+      <Flex direction="row" align="center" height="1.5em" gridGap={0}>
+        {colors.map((color) => {
+          const colorHex = `#${rgbHex(color[0], color[1], color[2])}`;
+          return (
+            <Flex key={colorHex} flex="1" background={colorHex}>
+              &nbsp;
+            </Flex>
+          );
+        })}
+        <Flex>
+          <IconButton
+            icon={
+              hasCopied ? (
+                <FaClipboardCheck aria-label="copied" />
+              ) : (
+                <FaClipboard onClick={onCopy} />
+              )
+            }
+            aria-label="copy palette"
+          />
+        </Flex>
+      </Flex>
+    </Skeleton>
+  );
+};
 
 /**
  * @deprecated don't use. Use your image's src directly
