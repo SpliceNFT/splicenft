dc6538e1c091d78ad6569390e4ad49197b414d14
Stefan <stadolf@gmail.com>
2021-12-29 15:53:19 +0100

    #153 fixes test cases consolidated dependencies

--- packages/common/test/rgbhex.ts
+++ packages/common/test/rgbhex.ts
@@ -1,8 +1,8 @@
 import { expect } from 'chai';
-import { rgbHex } from '../src';
+import { rgbToHex } from '../src';
 
 it('encodes rgb values as hex', async function () {
-  expect(rgbHex(255, 255, 255)).to.eq('ffffff');
-  expect(rgbHex(255, 128, 0)).to.eq('ff8000');
-  expect(rgbHex(0, 0, 0)).to.eq('000000');
+  expect(rgbToHex([255, 255, 255])).to.eq('ffffff');
+  expect(rgbToHex([255, 128, 0])).to.eq('ff8000');
+  expect(rgbToHex([0, 0, 0])).to.eq('000000');
 });
