6d5ff9a02a8c2312640297f44a927718d6463967
stadolf <stadolf@gmail.com>
2023-12-27 21:27:57 +0100

    loads wasm on the fly in next
    Signed-off-by: stadolf <stadolf@gmail.com>

--- dominant-palette-kmeans-wasm/demo-app/src/pages/index.tsx
+++ dominant-palette-kmeans-wasm/demo-app/src/pages/index.tsx
@@ -1,48 +1,54 @@
-import { Color, get_dominant_colors } from 'dominant-palette-kmeans';
+import { Color } from 'dominant-palette-kmeans';
 import { ChangeEvent, useCallback, useState } from 'react';
 
 export default function Home() {
 
-  const [imageData, setImageData] = useState<{bytes: Uint8Array, preview: string}>();
-  const [palette, setPalette] = useState<Color[]>();
+    const [imageData, setImageData] = useState<{bytes: Uint8Array, preview: string}>();
+    const [palette, setPalette] = useState<Color[]>();
 
-  const onSelected = (e: ChangeEvent<HTMLInputElement>) => {
-    if (!e.target.files) return;
-    const selectedFile = e.target.files[0]
-    const reader = new FileReader();
-    reader.onload = (event) => {
-      if (!event?.target?.result) return;
-      const blob = new Blob([event.target.result], { type: selectedFile.type });
-      setImageData({
-        bytes: new Uint8Array(event.target.result as ArrayBufferLike), 
-        preview: URL.createObjectURL(blob)
-      });
+    const onSelected = (e: ChangeEvent<HTMLInputElement>) => {
+      if (!e.target.files) return;
+      const selectedFile = e.target.files[0]
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        if (!event?.target?.result) return;
+        const blob = new Blob([event.target.result], { type: selectedFile.type });
+        setImageData({
+          bytes: new Uint8Array(event.target.result as ArrayBufferLike), 
+          preview: URL.createObjectURL(blob)
+        });
+      };
+      reader.readAsArrayBuffer(selectedFile);
     };
-    reader.readAsArrayBuffer(selectedFile);
-  };
 
-  const extractColors = useCallback(() => {
-    if (!imageData) return;  
-    const palette = get_dominant_colors(imageData.bytes, 2, 10, 20).then(setPalette)
-  }, [imageData, setPalette])
+    const extractColors = useCallback(async () => {
+      if (!imageData) return;  
+      console.time("palette");
+      const { get_dominant_colors } = await import('dominant-palette-kmeans');
 
-  return (
-    <main>
-      <input type="file" id="file" name="file" onChange={onSelected}/>
-      <button onClick={extractColors}>extract dominant colors</button>
-      <p>
-      {palette && palette.map((c, i) => {
-        return <span key={`c-${i}`} style={{
-          display: "inline-block", 
-          height: "40px",
-          width:`${c.freq * 800}px`, 
-          background: c.hex
-        }}>
-        </span>
-      }
-      )}
-      </p>
-      { imageData && <img src={imageData.preview} alt="preview" width="400px" />  }
-    </main>
-  )
-}
+      get_dominant_colors(imageData.bytes, 2, 10, 20).then((pal) => {
+        console.timeEnd("palette")
+        setPalette(pal);
+      })
+    }, [imageData, setPalette])
+
+    return (
+      <main>
+        <input type="file" id="file" name="file" onChange={onSelected}/>
+        <button onClick={extractColors}>extract dominant colors</button>
+        <p>
+        {palette && palette.map((c, i) => {
+          return <span key={`c-${i}`} style={{
+            display: "inline-block", 
+            height: "40px",
+            width:`${c.freq * 800}px`, 
+            background: c.hex
+          }}>
+          </span>
+        }
+        )}
+        </p>
+        { imageData && <img src={imageData.preview} alt="preview" width="400px" />  }
+      </main>
+    )
+  }
